033d17c3b102dd73fbbe22d7854af608bd4de964
girishdev <girishkumar.aorg@gmail.com>
2018-10-24 07:50:36 +0530

    first commit

---
+++ Notes.php
@@ -0,0 +1,27 @@
+
+Laravel 5.5 API Resources
+
+For Bulding api in 5.5 Using API Resources
+https://www.youtube.com/watch?v=4pc6cgisbKE&t=1947s
+
+// list all
+http://localhost:8000/api/articles
+
+// gives perticular article
+http://localhost:8000/api/article/3
+
+// delete article
+http://localhost:8000/api/article/4
+
+// Create Article
+http://localhost:8000/api/article
+
+----------------
+
+By Traversy Media:
+Laravel 5.5 API From Scratch Using Resources.mp4
+Full Stack Vue.js & Laravel.mp4
+
+----------------
+For Bulding VueJs:
+https://www.youtube.com/watch?v=DJ6PD_jBtU0
